6528bf7c44904dc0ca5ffe2ece4ce87114b3d540
a-sambhab <saisambhav21chaini@gmail.com>
2021-09-11 01:58:32 +0530

    Initial Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
-import React from "react"
+import React, {useState} from "react";
+import Form from "../components/form";
+import Result from "../components/result";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [dinoid, setdinoid] = useState(-1);
+  const fetchid = (dinoid) => {
+    setdinoid(dinoid);
+    // console.log(dinoid);
+  }
+  return (
+    <>
+      <Form
+        passnote={fetchid}
+      />
+      <Result
+        dinoind={dinoid}
+      />
+    </>
+  )
 }
